e1fd8bcad6880eac748a21c578241d54ac0b4435
강인화 <164142264+kih1015@users.noreply.github.com>
2026-01-29 09:09:43 +0900

    [Bronze V] Title: 웰컴, Time: 100 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // [1단계] Welcome 출력
+        StringBuffer sb = new StringBuffer();
+        sb.append(".  .   .").append("\n")
+                .append("|  | _ | _. _ ._ _  _").append("\n")
+                .append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
